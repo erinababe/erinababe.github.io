440c39a9c68fb41ccd262b05517feb4276dcaeb9
erinababe <30730719+erinababe@users.noreply.github.com>
2017-08-05 12:44:52 +0800

    v1

---
+++ js/scripts.js
@@ -0,0 +1,107 @@
+
+
+/*-------------- scroll -----------------*/
+
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
+
+
+// create the back to top button
+$('body').prepend('<a href="#" class="back-to-top">Back to Top</a>');
+
+var amountScrolled = 300;
+
+$(window).scroll(function() {
+  if ( $(window).scrollTop() > amountScrolled ) {
+    $('a.back-to-top').fadeIn('slow');
+  } else {
+    $('a.back-to-top').fadeOut('slow');
+  }
+});
+
+$('a.back-to-top, a.simple-back-to-top').click(function() {
+  $('html, body').animate({
+    scrollTop: 0
+  }, 700);
+  return false;
+});
+
+
+/*-------------- accordion -----------------*/
+$('[data-toggle="collapse"]').click(function() {
+  $('.collapse.in').collapse('hide')
+});
+
+
+/*-------------- images fade in on scroll -----------------*/
+
+
+$(window).on("load",function() {
+    function fade() {
+        var animation_height = $(window).innerHeight() * 0.25;
+        var ratio = Math.round( (1 / animation_height) * 10000 ) / 10000;
+
+        $('.fade').each(function() {
+            
+            var objectTop = $(this).offset().top;
+            var windowBottom = $(window).scrollTop() + $(window).innerHeight();
+            
+            if ( objectTop < windowBottom ) {
+                if ( objectTop < windowBottom - animation_height ) {
+                    $(this).html( 'fully visible' );
+                    $(this).css( {
+                        transition: 'opacity 0.1s linear',
+                        opacity: 1
+                    } );
+
+                } else {
+                    $(this).html( 'fading in/out' );
+                    $(this).css( {
+                        transition: 'opacity 0.25s linear',
+                        opacity: (windowBottom - objectTop) * ratio
+                    } );
+                }
+            } else {
+                $(this).html( 'not visible' );
+                $(this).css( 'opacity', 0 );
+            }
+        });
+    }
+    $('.fade').css( 'opacity', 0 );
+    fade();
+    $(window).scroll(function() {fade();});
+});
